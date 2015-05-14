f857fac896cbe3b8ff419e13a09aa8dd7b32f658
Myles Horton <myles@getlantern.org>
2015-05-14 10:07:04 -0700

    simple deploy script closes #81

---
+++ deploy.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function die() {
+  echo $*
+  exit 1
+}
+
+${FIRETWEET_DOWNLOADS:?"FIRETWEET_DOWNLOADS must be set to the location of the firetweet/downloads repository"}
+
+#gradle || die "Could not build"
+cp bin/firetweet.apk $FIRETWEET_DOWNLOADS/ || die "Could not copy to downloads dir"
+cd $FIRETWEET_DOWNLOADS || die "Could not move to downloads dir"
+git add firetweet.apk || die "Could not add apk?"
+git commit -m "Updated version" 
+git push origin master || die "Could not push new FireTweet version!"
+cd -
