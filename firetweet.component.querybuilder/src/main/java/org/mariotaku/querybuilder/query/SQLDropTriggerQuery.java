89049374382bc31ab48e68d55c8c8773f0e0d548
Todd Blose <todd@lana.io>
2015-05-20 21:04:38 -0400

    remove old querybuilder and jsonserializer packages

--- firetweet.component.querybuilder/src/main/java/org/mariotaku/querybuilder/query/SQLDropTriggerQuery.java
+++
@@ -1,9 +0,0 @@
-package org.getlantern.querybuilder.query;
-
-public class SQLDropTriggerQuery extends SQLDropQuery {
-
-    public SQLDropTriggerQuery(final boolean dropIfExists, final String table) {
-        super(dropIfExists, "TRIGGER", table);
-    }
-
-}
