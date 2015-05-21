a358fe43d2188df5bb83c6e9c589164500f48459
Todd Blose <todd@lana.io>
2015-05-21 07:39:59 -0400

    remove additional files and unused modules

--- firetweet.extension.streaming/src/main/java/org/getlantern/firetweet/extension/streaming/Constants.java
+++ firetweet.extension.streaming/src/main/java/org/getlantern/firetweet/extension/streaming/Constants.java
@@ -4,7 +4,7 @@ import org.getlantern.firetweet.FiretweetConstants;
 
 public interface Constants extends FiretweetConstants {
 
-	public static final String LOGTAG = "Twidere.Streaming";
+	public static final String LOGTAG = "Firetweet.Streaming";
 
 	public static final String PREFERENCE_KEY_ACCOUNT_IDS = "account_ids";
 	public static final String PREFERENCE_KEY_ENABLE_STREAMING = "enable_streaming";
