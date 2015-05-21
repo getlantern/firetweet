37a511d8e52edaa88fa502169859e4696f2b536d
Todd Blose <todd@lana.io>
2015-05-20 20:01:07 -0400

    remove submodules

--- twidere.extension.twitlonger/src/main/java/org/mariotaku/twidere/extension/twitlonger/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.mariotaku.twidere.extension.twitlonger;
-
-public interface Constants {
-
-	public static final String TWITLONGER_APP_NAME = "twidere";
-	public static final String TWITLONGER_API_KEY = "h6c4LaVx5g7Entja";
-}
