a6ce82e6953c4227f9abcbdb85a81fed51698a0b
Myles Horton <myles@getlantern.org>
2015-05-31 14:15:33 -0700

    Added deploying to s3

--- deploy.bash
+++ deploy.bash
@@ -8,6 +8,7 @@ function die() {
 ${FIRETWEET_DOWNLOADS:?"FIRETWEET_DOWNLOADS must be set to the location of the firetweet/downloads repository"}
 
 #gradle || die "Could not build"
+s3cmd put -P bin/firetweet.apk s3://firetweet || die "Could not upload binary to S3?"
 cp bin/firetweet.apk $FIRETWEET_DOWNLOADS/ || die "Could not copy to downloads dir"
 cd $FIRETWEET_DOWNLOADS || die "Could not move to downloads dir"
 git add firetweet.apk || die "Could not add apk?"
